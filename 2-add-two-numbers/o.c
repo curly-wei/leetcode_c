93779848ad6dcbd74424ba8ab0a987806a400604
curlywei <curlywei@gmail.com>
2021-06-13 15:56:11 +0800

    1st

---
+++ 2-add-two-numbers/o.c
@@ -0,0 +1,34 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
+  struct ListNode* result;  
+  volatile bool carry = false;
+  while ((l1 != NULL) || (l2 != NULL) ) {
+    result = (struct ListNode*)malloc(sizeof(struct ListNode) );
+    result->next = NULL;
+    int v1 = 0;     
+    if (l1 != NULL) {
+      v1 = l1->val;
+      l1 = l1->next;
+    }
+    
+    int v2 = 0;     
+    if (l2 != NULL) {
+      v2 = l2->val;
+      l2 = l2->next;
+    }
+        
+    int sum = v1 + v2;
+    if (carry == true)
+      sum += 1;
+    carry = (sum > 9);
+        
+    result->val = sum%10;
+    printf("result->val: %d, sum: %d, carry: %d\n",result->val,sum,carry); 
+    //result = result->next;
+
+  }
+  
+  return result;
+}
---
+++ 2-add-two-numbers/t.c
@@ -0,0 +1,42 @@
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
+  struct ListNode *result = NULL; // Initialize the variable.
+  struct ListNode *last = NULL;
+  bool carry = false; // There is no need for this to be volatile.
+  while ((l1 != NULL) || (l2 != NULL) ) {
+    struct ListNode *temp = malloc(sizeof(struct ListNode)); // You shouldn't cast the result of malloc.
+    if ( !temp ) {
+        // handle the error somehow
+    }
+    temp->next = NULL;
+    int v1 = 0;     
+    if (l1 != NULL) {
+      v1 = l1->val;
+      l1 = l1->next;
+    }
+    
+    int v2 = 0;     
+    if (l2 != NULL) {
+      v2 = l2->val;
+      l2 = l2->next;
+    }
+        
+    int sum = v1 + v2;
+    if (carry)
+      sum++;
+    carry = (sum > 9);
+        
+    temp->val = sum%10;
+    printf("result->val: %d, sum: %d, carry: %d\n",temp->val,sum,carry); 
+
+    if ( last ) {
+      last->next = temp;
+    }
+    else {
+      result = temp;
+    }
+    last = temp;
+  }
+  
+  return result;
+}
+
